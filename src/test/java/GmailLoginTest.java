c4f4c3fdc1b5689ad99264180674e7c28cff0b57
vverma608 <vikas.vatsh@gmail.com>
2017-03-20 18:06:05 +0530

    This is intial check-in

---
+++ src/test/java/GmailLoginTest.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GmailLoginTest {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://gmail.com");
+		driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("Email")).sendKeys("hadoopvverma@gmail.com");
+		driver.findElement(By.id("next")).click();
+		
+		
+
+	}
+
+}
